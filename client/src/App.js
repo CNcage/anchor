9eb0384a9484590dcd48d6f3522121a2a00d8543
Robert <info@jmkit.com>
2020-06-30 15:42:41 +0100

    Name fields added
    Name fields added

--- client/src/App.js
+++ client/src/App.js
@@ -2,50 +2,55 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
     state = {
-      form_email: '',
-      form_password:''
+        form_name: '',
+        form_email: '',
+        form_password: ''
     }
-    formData = (e) =>{
+    formData = (e) => {
         this.setState({
             ...this.state,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         })
     }
-    submitForm = async(e) => {
+    submitForm = async (e) => {
         e.preventDefault();
         const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
+            headers: {
+                'Content-Type': 'application/json'
+            }
         }
         const body = JSON.stringify({
             email: this.state.form_email,
             password: this.state.form_password
-        }) 
+        })
         const newreg = await axios.post('/register', body, config);
         const newregd = await axios.get('/registered', body, config);
-        
+
         console.log(newreg.data);
-        console.log(newregd.data); 
+        console.log(newregd.data);
     }
 
 
-    render(){
-        return(
-        <div>
-            <form onSubmit={this.submitForm}>
-                <label htmlFor = "form_email">email address:</label>
-                <input type = "email" name = "form_email" onChange = {this.formData}/>
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submitForm}>
+                    <label htmlFor="form_name">Name:</label>
+                    <input type="name" name="form_name" onChange={this.formData} />
 
-                <label htmlFor = "form_password">Password:</label>
-                <input type = "password" name = "form_password" onChange = {this.formData}/>
+                    <label htmlFor="form_email">email address:</label>
+                    <input type="email" name="form_email" onChange={this.formData} />
 
-                <button type = "submit">Register User</button>
-            </form>
-        </div>
-    )}
+                    <label htmlFor="form_password">Password:</label>
+                    <input type="password" name="form_password" onChange={this.formData} />
+
+                    <button type="submit">Register User</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 
--- index.js
+++ index.js
@@ -12,10 +12,10 @@ mongoose.connect(keys.mongoURI, {
     useUnifiedTopology: true
 });
 
-app.use (express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/register", async(req, res) => {
+app.post("/register", async (req, res) => {
     const user = new User(req.body);
     await user.save();
     res.status(200).json({
@@ -23,13 +23,13 @@ app.post("/register", async(req, res) => {
     })
 })
 
-app.get('/registered', (req, res)=>{
+app.get('/registered', (req, res) => {
     console.log("loading registered")
     res.status(200).json({
-    message: 'User registered',
-    user: 'new user'
+        message: 'User registered',
+        user: 'new user'
     })
-    })
+})
 
 
 const PORT = process.env.PORT || 5000;
