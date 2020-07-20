d48e156ea6bbb2afd109eb3bad1a9ebcea762e79
Bex Birkett <bex.birkett@outlook.com>
2020-07-20 13:41:52 +0100

    heroku update- changed exception

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ mongoose.connect(keys.mongoURI, {
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT)
 
-if (process.env.NODE_ENV === "production") {
-
-  app.use(express.static("build"));
 
 
-  app.get("*", (req, res) => {
-       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-   });
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res)=> {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 }
