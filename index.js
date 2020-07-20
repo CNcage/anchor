fe0b46ceee861d25fa2aeef1c3822b3acb6a0e53
Bex Birkett <bex.birkett@outlook.com>
2020-07-20 13:30:07 +0100

    heroku update- added production exception

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ mongoose.connect(keys.mongoURI, {
 // App listen
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT)
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("build"));
+
+
+  app.get("*", (req, res) => {
+       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+   });
+
+}
